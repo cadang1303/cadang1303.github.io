d74ad000658d5d91f37fa6bf16073f2817b633a8
cadang1303 <56860449+cadang1303@users.noreply.github.com>
2021-05-22 13:38:55 +0700

    commit 5

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,23 @@
-const btn = document.getElementById("btn");
-var scrollTop = 0;
+document.addEventListener("scroll", handleScroll);
 
-if(window.scrollBy(100,100)) {
-    btn.classList.toggle("hide");
+var scrollToTopBtn = document.querySelector(".btn");
+
+function handleScroll() {
+  var scrollableHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var ratio = 0.3;
+
+  if ((document.documentElement.scrollTop / scrollableHeight ) > ratio) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
 }
 
-if(window.pageYOffset = scrollTop){
-    btn.classList.add("hide");
+scrollToTopBtn.addEventListener("click", scrollToTop);
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
 }
